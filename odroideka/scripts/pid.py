d21407d68a9ab2a76ee42f36cf4a535f79fe4d0d
Marissa Tracy <matr4187@colorado.edu>
2019-03-16 13:53:56 -0600

    Added pid controller function

--- odroideka/scripts/pid.py
+++ odroideka/scripts/pid.py
@@ -6,9 +6,33 @@ from odroideka.msg import Command
 
 command_pub = rospy.Publisher('command', Command, queue_size=10)
 
+# Will need to update these
+dist_prop = 0.5
+max_dist = 1024
+max_steer = 40
+steer_prop = 0.3
+max_speed  = .25
+
 class PID():
-    def __init__(self):
-        pass
+
+    def pidcontroller(dist, steer_angle):
+        global dist_prop
+        global max_dist
+        global max_steer
+        global steer_prop
+        global max_speed
+
+        # Possibly update later if we want to change the desired distance
+        des_dist = 200 
+        err = dist - des_dist
+        # print(err)
+        # Set to a constant for now, will change later
+        des_speed = .25 #abs((err * dist_prop) / max_dist)
+        # des_steer_angle = steer_angle / max_steer
+        des_steer = (((err * steer_prop) * ((max_steer - steer_angle) / max_steer)))/40
+        print("Distance: {}, Steering Angle: {}, Desired Speed: {}, Desired Steering Angle: {}".format(dist, steer_angle, des_speed, des_steer))
+        
+        return des_speed, des_steer
 
 #distance data comes through here
 def callback(data):
