188a6161fcc99907d11a79c90a973fd409a0f302
bubba311 <ryib0384@colorado.edu>
2019-03-18 16:15:39 -0600

    Update pid.py

--- odroideka/scripts/pid.py
+++ odroideka/scripts/pid.py
@@ -3,6 +3,7 @@ import rospy
 from std_msgs.msg import String
 from odroideka.msg import Distance
 from odroideka.msg import Command
+import message_filters
 
 command_pub = rospy.Publisher('command', Command, queue_size=10)
 
@@ -13,48 +14,46 @@ max_steer = 40
 steer_prop = 0.3
 max_speed  = .25
 
-class PID():
 
-    def pidcontroller(dist, steer_angle):
-        global dist_prop
-        global max_dist
-        global max_steer
-        global steer_prop
-        global max_speed
+def pidcontroller(dist, cmd):
+    global dist_prop
+    global max_dist
+    global max_steer
+    global steer_prop
+    global max_speed
+    steer_angle = cmd.turn * max_steer
+    # Possibly update later if we want to change the desired distance
+    des_dist = 200 
+    err = dist.right - des_dist
+    # print(err)
+    # Set to a constant for now, will change later
+    des_speed = .25 #abs((err * dist_prop) / max_dist)
+    # des_steer_angle = steer_angle / max_steer
+    des_steer = (((err * steer_prop) * ((max_steer - steer_angle) / max_steer)))/max_steer
+    #print("Distance: {}, Steering Angle: {}, Desired Speed: {}, Desired Steering Angle: {}".format(dist, steer_angle, des_speed, des_steer))
 
-        # Possibly update later if we want to change the desired distance
-        des_dist = 200 
-        err = dist - des_dist
-        # print(err)
-        # Set to a constant for now, will change later
-        des_speed = .25 #abs((err * dist_prop) / max_dist)
-        # des_steer_angle = steer_angle / max_steer
-        des_steer = (((err * steer_prop) * ((max_steer - steer_angle) / max_steer)))/40
-        print("Distance: {}, Steering Angle: {}, Desired Speed: {}, Desired Steering Angle: {}".format(dist, steer_angle, des_speed, des_steer))
-        
-        return des_speed, des_steer
-
-#distance data comes through here
-def callback(data):
-    print("Got: ", data.dist) 
-
-def main():
-    dist_sub = rospy.Subscriber('filtered_distance', Distance, callback)
-    rospy.init_node('pid', anonymous=False)
-    #test send_command 
-    while not rospy.is_shutdown():
-        send_command()
-
-
-#send speed and turn commands b/w [-1, 1]
-def send_command():
+    #Give to pololu
     msg = Command()
-    msg.speed = 0.5
-    msg.turn = -0.5
+    msg.speed = des_speed
+    msg.turn = des_steer
     command_pub.publish(msg)
 
+    return
+
+def main():
+    #Get filtered distance data and state data
+    dist_sub = rospy.Subscriber('filtered_distance', Distance)
+    state = rospy.Subscriber('state', Command)
+    
+    #Time synchronize it
+    ts = message_filters([dist_sub, state], 10)
+    ts.registerCallback(pidcontrolller)
+    
+    rospy.init_node('pid', anonymous=False)
+    rospy.spin()
+
+
 if __name__ == '__main__':
-    pid = PID()
     try:
         main()
     except rospy.ROSInterruptException:
