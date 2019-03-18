538deed387b5a339788981c611e4f01397e2d164
SumeetBatra <sscifi@gmail.com>
2019-03-18 21:56:44 +0000

    Added state publisher

--- odroideka/scripts/pololu.py
+++ odroideka/scripts/pololu.py
@@ -16,6 +16,18 @@ steering_pin = 5
 left_pin = 11
 right_pin = 2
 
+def initialize():
+    global board
+    global throttle_pin
+    global steering_pin
+    
+    board.setTarget(throttle_pin,6000)
+    board.setTarget(steering_pin,6000)
+
+    return
+
+    
+
 def send_command(cmd):
     global board
     global throttle_pin
@@ -46,20 +58,35 @@ def convert2dist(bytes):
     distance = 195300/((8*bytes)-1767)
     return distance
  
+def get_state()
+    global board
+    global throttle_pin
+    global steering_pin
+    vel = board.getPosition(throttle_pin)
+    ang = board.getPosition(steering_pin)
+    vel = (vel - 6000)*0.001
+    ang = (ang - 6000)*0.001
+    return vel, ang
 
 def main():
+    initialize()
     dist_pub = rospy.Publisher('distance', Distance, queue_size=10)
     command_sub = rospy.Subscriber('command', Command, send_command)
+    command_pub = rospy.Publisher('state', Command, queue_size=10)
     rospy.init_node('pololu', anonymous=True)
     rate = rospy.Rate(60) #10hz, can modify this later
     while not rospy.is_shutdown():
         dist = get_distance()
+        state = get_state()
+        command_pub.publish(*state)
         dist_pub.publish(*dist)
         rate.sleep()
+    initialize()
 
 if __name__ == '__main__':
     try:
         main()
-    except rospy.ROSInterruptException:
+    except:
+        initialize()
         print("Sumeet wasn't here")
 
